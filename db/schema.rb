d3213a9342e20d2f0c9e18fcf51d33cd33cadbf0
uche-inyama <depoulet@yahoo.com>
2019-09-18 14:26:32 +0100

    add remember_digest to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_101738) do
+ActiveRecord::Schema.define(version: 2019_09_18_122148) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_09_18_101738) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end
