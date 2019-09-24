44cc5ad83841f944ee73bebb60ef67436965bd41
uche-inyama <depoulet@yahoo.com>
2019-09-24 11:18:56 +0100

    add validation for event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+    before(:each)do
+      @user = User.create!(name: "Moses", email: "moses@gmail.com", password: "asdf4321")
+      @event = Event.create!(name: "event_name", date: "05-12-2019", creator_id: 1)
+    end
+
+    describe "creation" do
+      it "should have one item created after being created" do
+        expect(Event.all.count).to eq(1)
+      end
+    end
+
+    describe "validations" do
+      it "is not valid without an event_name" do
+        @event.name = nil
+        expect(@event).to_not be_valid
+      end
+
+      it "is not valid without a date" do
+        @event.date = nil
+        expect(@event).to_not be_valid
+      end
+    end
 end
+
