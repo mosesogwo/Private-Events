11918e2ff5dbbb0f4a63942348173cc43aa59351
Moses Ogwo <mosesogwo@gmail.com>
2019-09-24 13:23:41 +0100

    uncomment sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,16 @@ class SessionsController < ApplicationController
   def new
   end
 
-  # def create
-  #   @user = User.find_by(email: params[:session][:email])
-  #   if @user && @user.authenticate(params[:session][:password])
-  #     log_in @user
-  #     remember @user
-  #     flash[:success] = "You're logged in"
-  #     redirect_to user_path @user
-  #   else
-  #     flash[:danger] = "Error logging in"
-  #     render 'new'
-  #   end
-  # end
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      remember @user
+      flash[:success] = "You're logged in"
+      redirect_to user_path @user
+    else
+      flash[:danger] = "Error logging in"
+      render 'new'
+    end
+  end
 end
