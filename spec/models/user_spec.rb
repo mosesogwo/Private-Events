5039f4e683b92d5344420a02d7c2dbf650eb24c6
uche-inyama <depoulet@yahoo.com>
2019-09-24 10:59:32 +0100

    add presence validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it "is not valid without a name" do
-        user = User.new(name: "")
-        expect(user).to_not be_valid
+    before(:each)do
+        @user = User.create!(name: "Uche", email: "uche@gmail.com", password: "123asdf")
+    end
+
+    describe "creation" do
+        it "should have one item created after being created" do
+            expect(User.all.count).to eq(1)
+        end
+    end
+    describe "validations" do
+        it "is not valid without a name" do
+            @user.name = nil
+            expect(@user).to_not be_valid
+        end
+
+        it "is not valid without an email" do
+            @user.email = nil
+            expect(@user).to_not be_valid
+        end
+
+        it "is not valid without a password" do
+            @user.password = nil
+            expect(@user).to_not be_valid
+        end
     end
 end
