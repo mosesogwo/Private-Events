474b6603d8c6fe9640ee9bb2854b703963eb6f99
Moses Ogwo <mosesogwo@gmail.com>
2019-09-27 12:22:50 +0100

    add validation for creating event in the past

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@
 class Event < ApplicationRecord
   validates :name, presence: true
   validates :date, presence: true
+  validate :event_date_cannot_be_in_the_past
 
   belongs_to :creator, class_name: 'User'
   has_many :attendances, foreign_key: 'attended_event_id'
@@ -10,4 +11,10 @@ class Event < ApplicationRecord
 
   scope :past, -> { where('date <= ?', Date.today) }
   scope :upcoming, -> { where('date > ?', Date.today) }
+
+  def event_date_cannot_be_in_the_past
+    if date < Date.today
+      errors.add(:date, "Can't be in the past")
+    end
+  end
 end
